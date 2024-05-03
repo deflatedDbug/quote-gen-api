4f531a39a76e8cbd7c9dc9f747bf8470ddddfc83
deflatedDbug <subinlebow2@gmail.com>
2024-05-03 13:44:28 -0400

    preparing for Heroku deployment

--- app.py
+++ app.py
@@ -406,4 +406,5 @@ def get_data():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
